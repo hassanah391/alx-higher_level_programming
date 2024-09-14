b9c8a79eb840a16876f9ff8f3b9ebdd0680e9624
Hassan Ahmed <hassan.ahmed3577@gmail.com>
2024-09-14 22:31:57 +0300

    correct the right output in 5

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 for i in range(0, 100):
     if i < 10:
-        print("0{}, ".format(i), end="")
-    elif i != 99:
-        print(f"{i}, ", end="")
+        print("0{}".format(i), end=", ")
     else:
-        print('99')
+        print("{}".format(i), end=", " if i != 99
+              else "\n")
