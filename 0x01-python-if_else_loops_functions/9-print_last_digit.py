a9fb410e212766cae70473adda67699169db7080
Hassan Ahmed <hassan.ahmed3577@gmail.com>
2024-09-15 13:49:32 +0300

    Add 9

---
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+
+
+def print_last_digit(number):
+    print("{}".format(number % 10) if number > 0 else ((-1 * number) % 10),
+          end="")
+    return number % 10 if number > 0 else ((-1 * number) % 10)
