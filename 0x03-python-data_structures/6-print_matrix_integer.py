4e7b5c9d5f5ab73340a5f6383498ea8a433b607d
Hassan Ahmed <hassan.ahmed3577@gmail.com>
2024-09-19 06:14:24 +0300

    remove the space in the end of each line

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
     for i in range(0, len(matrix)):
-        for j in matrix[i]:
-            print("{:d}".format(j), end=" ")
+        for j in range(0, len(matrix[i])):
+            print("{:d}".format(matrix[i][j]), end=" "
+                  if j != len(matrix[i]) else "\n")
         print("")
