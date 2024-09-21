437c42bc25e5849e259116eb2464ab429bf08815
Hassan Ahmed <hassan.ahmed3577@gmail.com>
2024-09-21 15:12:43 +0300

    Adds 2

---
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+def uniq_add(my_list=[]):
+    uniq_numbers = [my_list[0]]
+    is_uniq = True
+    sum = my_list[0]
+    for i in range(1, len(my_list)):
+        for j in range(0, len(uniq_numbers)):
+            if my_list[i] == uniq_numbers[j]:
+                is_uniq = False
+        if is_uniq:
+            uniq_numbers.append(my_list[i])
+            sum += my_list[i]
+        else:
+            is_uniq = True
+    return sum
