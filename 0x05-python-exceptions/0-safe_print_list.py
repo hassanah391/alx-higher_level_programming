c3171d9094024f940ea3ef5bb899d10011571003
Hassan Ahmed <hassan.ahmed3577@gmail.com>
2024-10-01 07:46:38 +0300

    Handle if x = 0

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -4,11 +4,12 @@ def safe_print_list(my_list=[], x=0):
     real_number = 0
 
     try:
-        for real_number in range(x):
-            print("{}".format(my_list[real_number]), end="")
+        for i in range(x):
+            print("{}".format(my_list[i]), end="")
+            real_number += 1
         print("")
     except IndexError:
         print("")
         return real_number
     else:
-        return real_number + 1
+        return real_number
