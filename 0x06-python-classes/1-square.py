89e915cdba42e0e9ba18b281a03f534f73dbb991
Hassan Ahmed <hassan.ahmed3577@gmail.com>
2024-10-04 15:50:51 +0300

    Adds 1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+""" This module is for creating a square class """
+
+
+class Square:
+    """ a class for creating square """
+    def __init__(self, s):
+        self.__size = s
