df4e7ef657b9eece8cd66297184f07eb1f9635bd
Hassan Ahmed <hassan.ahmed3577@gmail.com>
2024-10-08 10:32:48 +0300

    Adds 2

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -43,3 +43,9 @@ class Rectangle:
             raise ValueError("height must be >= 0")
         else:
             self.__height = height
+
+    def area(self):
+        return self.__width * self.__height
+
+    def perimeter(self):
+        return 2 * (self.__width + self.__height)
