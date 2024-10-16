ca218a45ea7bc28ed64c4166fe3b9a33f8a70f81
Hassan Ahmed <hassan.ahmed3577@gmail.com>
2024-10-16 15:08:45 +0300

    Adds 4

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+
+Module 4-inherits_from contains:
+
+function inherits_from returns True if the object is an instance of a class
+that inherited (directly or indirectly) from the specified class ;
+otherwise False.
+"""
+
+
+def inherits_from(obj, a_class):
+    """
+
+    Return:
+    True if the object is an instance of a class that inherited
+    (directly or indirectly) from the specified class
+    """
+    return (type(obj) is not a_class and issubclass(type(obj), a_class))
