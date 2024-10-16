0b9ead3aa3621438e56771cc82fb66ca0b18a1cc
Hassan Ahmed <hassan.ahmed3577@gmail.com>
2024-10-16 20:22:20 +0300

    aas

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -33,4 +33,5 @@ class Square(Rectangle):
         self.__size = size
 
         def __str__(self):
-            return f"[Square] {self.__size}/{self.__size}"
+            return "[{:s}] {:d}/{:d}".format(self.__class__.__name__,
+                                         self.__size, self.__size)
