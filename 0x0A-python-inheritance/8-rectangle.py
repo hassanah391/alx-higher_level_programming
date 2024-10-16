8d93bac07d07cc9133618b25ecf0702e346d2bb3
Hassan Ahmed <hassan.ahmed3577@gmail.com>
2024-10-16 19:03:56 +0300

    Remove some indentations for pycodestyle

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -15,17 +15,17 @@ BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
 class Rectangle(BaseGeometry):
-     """inherits from BaseGeometry
+    """inherits from BaseGeometry
     Methods:
         __init__(self, width, height)
     """
-     def __init__(self, width, height):
-         """validate and initialize width and height
+    def __init__(self, width, height):
+        """validate and initialize width and height
         Args:
             width (int): private
             height (int): private
         """
-         super().integer_validator("width", width)
-         self.__width = width
-         super().integer_validator("height", height)
-         self.__height = height
+        super().integer_validator("width", width)
+        self.__width = width
+        super().integer_validator("height", height)
+        self.__height = height
