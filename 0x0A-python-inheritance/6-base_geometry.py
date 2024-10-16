c2c19a5efeb8b1e8b18bbee255d9e88769b8d284
Hassan Ahmed <hassan.ahmed3577@gmail.com>
2024-10-16 17:38:30 +0300

    Adds 6

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+
+Module 6-base_geometry contains:
+an empty class called BaseGeometry
+with public instance method area
+"""
+
+
+class BaseGeometry:
+    """
+
+    Methods:
+            area(self)
+    """
+    def area(self):
+        """ not implemented """
+        raise Exception("area() is not implemented")
