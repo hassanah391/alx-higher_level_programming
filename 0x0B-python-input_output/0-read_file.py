e2d931504083be8e92f7642d6b6902036a06131e
Hassan Ahmed <hassan.ahmed3577@gmail.com>
2024-10-18 20:26:09 +0300

    Adds 0

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+
+Module 0-read_file contains:
+read_file: a function to read a text file and prints
+          its content.
+"""
+
+
+def read_file(filename=""):
+    """ reads from a text file and prints its content
+    Args:
+        filename (string): the name of the file to read
+    """
+    with open(filename, 'r', encoding="utf-8") as f:
+        print(f.read())
---
+++ 0x0B-python-input_output/README.md
@@ -0,0 +1,1 @@
+Readme file for passing tests
