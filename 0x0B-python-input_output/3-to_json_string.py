1f22258cf214ae7196c0f85bc56e0efa12659541
Hassan Ahmed <hassan.ahmed3577@gmail.com>
2024-10-19 12:43:13 +0300

    fix pycodestyle

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
-import json
 """
 
 Module 3-to_json_string contains:
 - to_json_string: a function that returns the JSON representation of an object
 """
+import json
 
 
 def to_json_string(my_obj):
