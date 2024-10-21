2ed3f7fa5359c2ed0049cc80d120c236639fb3fd
Hassan Ahmed <hassan.ahmed3577@gmail.com>
2024-10-21 18:21:01 +0300

    Adds base module and test

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+
+Module base contains:
+
+Class Base:
+private class attribute __nb_objects = 0
+"""
+
+
+class Base:
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        if id != None:
+            self.id = id
+        else
+        __nb_objects += 1
