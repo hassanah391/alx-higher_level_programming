99cdd5a8f6272bb0c9804af0c0612e87349645a0
Hassan Ahmed <hassan.ahmed3577@gmail.com>
2024-10-22 16:17:07 +0300

    Handles ValueError and TypeError exceptions

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -50,8 +50,10 @@ class Rectangle(Base):
 
     @width.setter
     def width(self, value):
-        if not value > 0:
-            raise ValueError("Width should be a positive number")
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        elif value <= 0:
+            raise ValueError("width must be > 0")
         else:
             self.__width = value
 
@@ -61,10 +63,12 @@ class Rectangle(Base):
 
     @height.setter
     def height(self, value):
-        if not value > 0:
-            raise ValueError("height should be a positive number")
+        if type(value) is not int:
+           raise TypeError("height must be an integer")
+        elif value <= 0:
+            raise ValueError("height must be > 0")
         else:
-            self.__height = value
+             self.__height = value
 
     @property
     def x(self):
@@ -72,8 +76,10 @@ class Rectangle(Base):
 
     @x.setter
     def x(self, value):
-        if not value > 0:
-            raise ValueError("X should be a positive number")
+        if type(value) is not int:
+            raise TypeError("x must be an integer")
+        elif value < 0:
+            raise ValueError("x must be >= 0")
         else:
             self.__x = value
 
@@ -83,7 +89,9 @@ class Rectangle(Base):
 
     @y.setter
     def y(self, value):
-        if not value > 0:
-            raise ValueError("Y should be a positive number")
+        if type(value) is not int:
+            raise TypeError("y must be an integer")
+        elif value < 0:
+            raise ValueError("y must be >= 0")
         else:
             self.__y = value
