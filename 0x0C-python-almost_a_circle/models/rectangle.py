d81b9b03fb421574f6630f4d25cd0d7245b17349
Hassan Ahmed <hassan.ahmed3577@gmail.com>
2024-10-22 17:48:35 +0300

    Adds display()

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -124,3 +124,7 @@ class Rectangle(Base):
     def area(self):
         """returns the area value of the Rectangle instance"""
         return self.width * self.height
+
+    def display(self):
+        for _ in range(self.height):
+            print("#" * self.width)
