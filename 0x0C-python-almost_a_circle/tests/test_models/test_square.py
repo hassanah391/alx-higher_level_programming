6d7175327848db24fe2765bb9e411b5a20cf5ab4
Hassan Ahmed <hassan.ahmed3577@gmail.com>
2024-10-24 16:36:39 +0300

    Adds test_square

---
+++ 0x0C-python-almost_a_circle/tests/test_models/test_square.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+
+A unit test for Square class, Run these commands:
+$ python3 -m unittest discover tests
+$ python3 -m unittest tests/test_models/test_square.py
+"""
+
+
+import unittest
+from models.square import Square
+
+
+class TestSquare(unittest.TestCase):
+    def setUp(self):
+        obj1 = Square(3, 1, 3)
+    
+    def test_str(self):
+        obj1 = Square(3, 1, 3, 3)
+        self.assertEquals(obj1.__str__(), "[Square] (3) 1/3 - 3")
