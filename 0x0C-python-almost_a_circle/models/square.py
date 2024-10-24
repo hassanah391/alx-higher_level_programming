40628b9570e871e0fb1110e2dafe3a6a8645801c
Hassan Ahmed <hassan.ahmed3577@gmail.com>
2024-10-24 17:34:51 +0300

    Adds getter and setter for size

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -27,5 +27,14 @@ class Square(Rectangle):
             # inherits Rectangle attributes
             super().__init__(id=id, width=size, height=size, x=x, y=y)
 
+    @property
+    def size(self):
+        return self.width
+
+    @size.setter
+    def size(self, value=0):
+        self.width = value
+        self.height = value
+
     def __str__(self):
         return f"[Square] ({self.id}) {self.x}/{self.y} - {self.width}"
