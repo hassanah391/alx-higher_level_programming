ec85caa5e5e63cca8fa7fdc0c798241844223eba
Hassan Ahmed <hassan.ahmed3577@gmail.com>
2024-10-24 17:41:43 +0300

    Adds update()

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -18,14 +18,9 @@ class Square(Rectangle):
     in module rectangle."""
     def __init__(self, size, x=0, y=0, id=None):
         """ initlizes instance attributes"""
-        # Validates size
-        if type(size) is not int:
-            raise TypeError("size must be an integer")
-        elif size <= 0:
-            raise ValueError("size must be > 0")
-        else:
             # inherits Rectangle attributes
-            super().__init__(id=id, width=size, height=size, x=x, y=y)
+        super().__init__(id=id, width=size, height=size, x=x, y=y)
+        self.size = size
 
     @property
     def size(self):
@@ -38,3 +33,29 @@ class Square(Rectangle):
 
     def __str__(self):
         return f"[Square] ({self.id}) {self.x}/{self.y} - {self.width}"
+    
+    def update(self, *args, **kwargs):
+        """
+        If args: set attributes in this order: id, width, height, x, y
+        If no args given: set attributes according to kwargs
+        """
+        if args:
+            for k, v in enumerate(args):
+                if k == 0:
+                    self.id = v
+                elif k == 1:
+                    self.size = v
+                elif k == 2:
+                    self.x = v
+                else:
+                    self.y = v
+        else:
+            if "id" in kwargs:
+                self.id = kwargs["id"]
+            if "size" in kwargs:
+                self.size = kwargs["size"]
+            if "x" in kwargs:
+                self.x = kwargs["x"]
+            if "y" in kwargs:
+                self.y = kwargs["y"]
+
