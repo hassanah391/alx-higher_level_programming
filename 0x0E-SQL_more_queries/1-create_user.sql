4bda0052788f88ee51f69d3cb9d8d15be6b37863
Hassan Ahmed <hassan.ahmed3577@gmail.com>
2024-12-26 00:07:36 +0200

    Add a script that create a user and give it all the privilages on Mysql server

---
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -0,0 +1,9 @@
+-- Creates a user called user_0d_1 with password user_0d_1_pwd
+CREATE USER IF NOT EXISTS'user_0d_1'@'localhost' IDENTIFIED
+WITH authentication_plugin BY 'user_0d_1_pwd';
+
+-- Gives user_0d_1 user all privileges on your MySQL server
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
+
+-- 3. Apply the changes
+FLUSH PRIVILEGES;
