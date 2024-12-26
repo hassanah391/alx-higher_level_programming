6618b2ae4ea85adb34a8c378504ea1eb75736c46
Hassan Ahmed <hassan.ahmed3577@gmail.com>
2024-12-26 04:20:13 +0200

    Correct the foregin key name

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -7,5 +7,5 @@ CREATE TABLE IF NOT EXISTS cities(
        id INT AUTO_INCREMENT UNIQUE NOT NULL PRIMARY KEY,
        state_id INT NOT NULL,
        name VARCHAR(256) NOT NULL,
-       FOREIGN KEY(id) REFERENCES states(id)
-)ENGINE=InnoDB;
+       FOREIGN KEY(state_id) REFERENCES states(id)
+) ENGINE=InnoDB;
