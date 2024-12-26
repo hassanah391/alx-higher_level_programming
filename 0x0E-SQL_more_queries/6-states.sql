7fd2d501a4f9d70650fd5a9128378d5522eef8f8
Hassan Ahmed <hassan.ahmed3577@gmail.com>
2024-12-26 04:00:46 +0200

    Add some comments

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -1,7 +1,8 @@
+-- Creates a hbtn_0d_usa database and use it
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
-
 USE hbtn_0d_usa;
 
+-- Creates states table
 CREATE TABLE IF NOT EXISTS states(
        id INT AUTO_INCREMENT UNIQUE NOT NULL PRIMARY KEY,
        name VARCHAR(256)
