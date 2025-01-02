49dc4c7b244a79e16d973cb836b5c7bb9954375f
Hassan Ahmed <hassan.ahmed3577@gmail.com>
2025-01-02 04:30:19 +0200

    follow pycodestyle

--- 0x0F-python-object_relational_mapping/relationship_state.py
+++ 0x0F-python-object_relational_mapping/relationship_state.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-This module defines the State class which is linked to the MySQL table "states".
+This module defines the State class which is linked to the MySQL table "states"
 """
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
